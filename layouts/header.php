e514d173eb43060355c2d2c98e96e8e9ffbac49a
yiox2048 <dabarayao@gmail.com>
2020-07-30 10:53:43 +0000

    modification du menu

--- layouts/header.php
+++ layouts/header.php
@@ -1,31 +1,25 @@
 <!-- ======= Header ======= -->
-  <header id="header" style="height: 80px;">
-    <div class="container d-flex">
+<header id="header" style="height: 80px;">
+  <div class="container d-flex">
 
-      <div class="logo mr-auto ">
-                <img class="img-fluid" src="assets/img/logo.png" style="width: 120px; height: 70px; "/> 
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-      </div>
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li class="active"><a href="index.html">Accueil</a></li>
-          <li><a href="#about">Actualité</a></li>
-          <li><a href="#services">Nos Services</a></li>
-          <li><a href="#portfolio">Nos évènements</a></li>
-          <li><a href="#pricing">Tarifs</a></li>
-          <li class="drop-down"><a href="">Notre communauté</a>
-            <ul>
-              <li><a href="#">Connnexion</a></li>
-              <li><a href="#">Nos réalisations</a></li>
-            </ul>
-          </li>
-          <li><a href="#contact">Contact</a></li>
-
-        </ul>
-      </nav><!-- .nav-menu -->
-
+    <div class="logo mr-auto ">
+      <img class="img-fluid" src="assets/img/logo.png" style="width: 120px; height: 70px; " />
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
     </div>
-  </header><!-- End Header -->
 
+    <nav class="nav-menu d-none d-lg-block">
+      <ul>
+        <li class="active"><a href="#">Accueil</a></li>
+        <li><a href="#">Présentation</a></li>
+        <li><a href="#">Nos Réalisations</a></li>
+        <li><a href="#">Paternship</a></li>
+        <li><a href="#">Forum</a></li>
+        <li><a href="#">Connexion</a></li>
+        <li><a href="#">Contact</a></li>
+
+      </ul>
+    </nav><!-- .nav-menu -->
+
+  </div>
+</header><!-- End Header -->
